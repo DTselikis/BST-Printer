98ffa0e30017539047b62432531f8ee2aebf2823
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-06-12 18:19:10 +0300

    Implemented a method that finds the number of children of every subtree

--- TreePrinter.cpp
+++ TreePrinter.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "TreePrinter.hpp"
+#include <vector>
 
 TreePrinter::TreePrinter(BST* tree) : tree(tree) {
 
@@ -12,4 +13,29 @@ int TreePrinter::countNodes(Node* root) {
 
 	// Starts from 1 to include current node
 	return 1 + countNodes(root->getLeftChild()) + countNodes(root->getRightChild());
+}
+
+std::vector<struct nodeInfo *> TreePrinter::nodesCounts(std::vector<Node*> levelOrder) {
+	std::vector<struct nodeInfo*> infos;
+
+	// For every node in the tree, calculate the number of the
+	// children of the left subtree and the number of children of the
+	// right subtree
+	for (auto& node : levelOrder) {
+		struct nodeInfo* info = new nodeInfo;
+
+		if (node != nullptr) {
+			info->leftCount = countNodes(node->getLeftChild());
+			info->rightCount = countNodes(node->getRightChild());
+		}
+		else {
+			// If the node does not exists, it will have zero children
+			info->leftCount = 0;
+			info->rightCount = 0;
+		}
+
+		infos.push_back(info);
+	}
+
+	return infos;
 }
--- TreePrinter.hpp
+++ TreePrinter.hpp
@@ -1,12 +1,18 @@
 #pragma once
 #include "BST.hpp"
 
+struct nodeInfo {
+	int leftCount = 0;
+	int rightCount = 0;
+};
+
 class TreePrinter {
 private:
 	BST *tree;
 
 	
 	int countNodes(Node* root);
+	std::vector<struct nodeInfo*> nodesCounts(std::vector<Node *> levelOrder);
 public:
 	TreePrinter(BST* tree);
 

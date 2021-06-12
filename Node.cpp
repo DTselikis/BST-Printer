76bbf805995a5b09c431d3a8ce15a96bf9b1d25a
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-06-12 13:45:42 +0300

    Implemented Node class

---
+++ Node.cpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "Node.hpp"
+
+Node::Node(int key, bool active) : key(key), leftChild(nullptr), rightChild(nullptr), active(active) {
+}
+
+Node::~Node() {
+	this->leftChild = nullptr;
+	this->rightChild = nullptr;
+}
+
+int Node::getKey() {
+	return this->key;
+}
+
+Node* Node::getLeftChild() {
+	return this->leftChild;
+}
+
+Node* Node::getRightChild() {
+	return this->rightChild;
+}
+
+bool Node::isActive() {
+	return this->active;
+}
+
+void Node::setLeftChild(Node* node) {
+	this->leftChild = node;
+}
+
+void Node::setRigthChild(Node* node) {
+	this->rightChild = node;
+}
+
+void Node::disable() {
+	this->active = false;
+}
---
+++ Node.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class Node {
+private:
+	int key;
+	Node* leftChild;
+	Node* rightChild;
+	bool active;
+public:
+	Node(int key, bool active = true);
+	~Node();
+
+	int getKey();
+	Node* getLeftChild();
+	Node* getRightChild();
+	bool isActive();
+
+	void setLeftChild(Node* node);
+	void setRigthChild(Node* node);
+	
+	void disable();
+};
